e3967044006dadc11baf88eec2dff1961ae25cfe
Feng Kuok <fengkuok@gmail.com>
2015-07-25 22:50:23 +0800

    demo for vertical

---
+++ src/main/resources/database/shard_0/jpetstore-hsqldb-schema.sql
@@ -0,0 +1,53 @@
+--
+--    Copyright 2010-2013 the original author or authors.
+--
+--    Licensed under the Apache License, Version 2.0 (the "License");
+--    you may not use this file except in compliance with the License.
+--    You may obtain a copy of the License at
+--
+--       http://www.apache.org/licenses/LICENSE-2.0
+--
+--    Unless required by applicable law or agreed to in writing, software
+--    distributed under the License is distributed on an "AS IS" BASIS,
+--    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+--    See the License for the specific language governing permissions and
+--    limitations under the License.
+--
+
+create table signon (
+    username varchar(25) not null,
+    password varchar(25)  not null,
+    constraint pk_signon primary key (username)
+);
+
+create table account (
+    userid varchar(80) not null,
+    email varchar(80) not null,
+    firstname varchar(80) not null,
+    lastname varchar(80) not null,
+    status varchar(2)  null,
+    addr1 varchar(80) not null,
+    addr2 varchar(40) null,
+    city varchar(80) not  null,
+    state varchar(80) not null,
+    zip varchar(20) not null,
+    country varchar(20) not null,
+    phone varchar(80) not null,
+    constraint pk_account primary key (userid)
+);
+
+create table profile (
+    userid varchar(80) not null,
+    langpref varchar(80) not null,
+    favcategory varchar(30),
+    mylistopt int,
+    banneropt int,
+    constraint pk_profile primary key (userid)
+);
+
+create table bannerdata (
+    favcategory varchar(80) not null,
+    bannername varchar(255)  null,
+    constraint pk_bannerdata primary key (favcategory)
+);
+
